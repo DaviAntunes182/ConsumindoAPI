4c073d9261c3454c260fc7433f644fac4420206e
DaviAntunes182 <davi.antunes@bb.com.br>
2024-11-03 17:07:51 -0300

    Gson -> Conversor de Json para Java, Record -> Criando classes imutáveis e sem comportamento, Criando um construtor que traduz um record para uma classe mutável

---
+++ screenmatch-v0.1.1/src/br/com/davi/screenmatch/main/ConsumindoAPI.java
@@ -0,0 +1,43 @@
+package br.com.davi.screenmatch.main;
+
+import br.com.davi.screenmatch.models.Filme;
+import br.com.davi.screenmatch.models.Obra;
+import br.com.davi.screenmatch.models.ObraOMDB;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class ConsumindoAPI {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o nome do filme");
+        String titulo = sc.nextLine();
+
+        String tituloParan = titulo.trim().replace(" ", "+");
+
+        String uri = "http://www.omdbapi.com/?t=" + tituloParan + "&apikey=c070cbb8";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
+        ObraOMDB obraOMDB = gson.fromJson(json, ObraOMDB.class);
+        Filme meuFilme = new Filme(obraOMDB);
+        System.out.println(meuFilme);
+    }
+}
